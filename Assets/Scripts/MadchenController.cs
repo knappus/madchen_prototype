b1a434f190bb059e43228f0c4746a55c598b0569
knappus <chris@fsinf.at>
2014-06-02 12:16:58 +0200

    added player sprite and basic movement

---
+++ Assets/Scripts/MadchenController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MadchenController : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    private bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+	   float move = Input.GetAxis("Horizontal");
+
+       rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move<0 && facingRight)
+            Flip();
+	}
+
+    void Flip() {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
