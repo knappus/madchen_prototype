e96cab4dcc19241d8ac1b198b8ce6d0f367043b5
Sophie <sophie.weiss1990@gmail.com>
2014-06-11 14:20:35 +0200

    added teddy shot

---
+++ Assets/Scripts/ShootingScript.js
@@ -0,0 +1,13 @@
+﻿var projectile : Rigidbody2D;
+var speed = 10;
+
+function Update () {
+
+if( Input.GetButtonDown("Fire1")) {
+
+	clone = Instantiate(projectile, transform.position, transform.rotation);
+	clone.velocity = transform.TransformDirection(Vector2(speed,0));
+	
+	Destroy (clone.gameObject, 5);
+
+}}
