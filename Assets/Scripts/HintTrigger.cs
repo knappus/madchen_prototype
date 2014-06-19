e4f793fb34611abd726b985bb3784fbe50153d85
Sophie <sophie.weiss1990@gmail.com>
2014-06-19 14:10:31 +0200

    added hint and trigger for lvl1

---
+++ Assets/Scripts/HintTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HintTrigger : MonoBehaviour {
+
+	public GameObject hint;
+
+	// Use this for initialization
+	void Start () {
+		hint.active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		
+		if (other.gameObject.name == "Player") {
+			Debug.Log ("Entering Hint Collider");
+			hint.active = true;
+			Destroy (hint, 5);
+		}
+	}
+
+}
